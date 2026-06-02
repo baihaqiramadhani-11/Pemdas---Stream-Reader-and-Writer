2c6aa324d3c0d690b54cc55c558563f8ec1a31bd
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-06-02 16:05:01 +0700

    catch

--- ManualExeption.cpp
+++ ManualExeption.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 using namespace std;
 
+   try
+    {
+        cout << "Selamat belajar di prodi TI UMY" << endl;
+        // throw 0.5; //melemparkan sebuah integer maka
+        cout << "Pernyataan tidak akan ditampilkan" << endl;
+    }
+    // kondisi jika throw yang bertipe data integer
+    catch (int a)
+    {
+        cout << "Pengecualian akan di eksekusi" << endl;
+    }
+    // kondisi jika throw bertipe data selain integer
