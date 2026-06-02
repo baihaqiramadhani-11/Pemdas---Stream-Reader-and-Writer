29e455a8d4de0388f24dc19a3170366a6b6316d1
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-06-02 16:10:02 +0700

    membuat file

--- IoFileWithPath.cpp
+++ IoFileWithPath.cpp
@@ -11,3 +11,8 @@ int main()
     // membuat input untuk menangkap nama file
     cout << "Masukkan nama file : ";
     cin >> NamaFile;
+
+    // membuat file dalam mode menulis
+    ofstream outfile;
+    
+    
